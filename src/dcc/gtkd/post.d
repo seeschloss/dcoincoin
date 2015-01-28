c7d3dc45290a02a1f681ba8e22dd494592af7e06
SeeSchloss <see.github@seos.fr>
2015-01-29 00:18:30 +0100

    The UI is beginning to look like a real UI

---
+++ src/dcc/gtkd/post.d
@@ -0,0 +1,137 @@
+module dcc.gtkd.post;
+
+private import std.string;
+
+private import dcc.engine.tribune;
+
+struct GtkPostSegmentContext {
+	bool bold = false,
+		 italic = false,
+		 underline = false,
+		 strike = false,
+		 fixed = false,
+		 clock = false,
+		 link = false,
+		 totoz = false;
+}
+
+struct GtkPostSegment {
+	GtkPostSegmentContext context;
+	string text;
+}
+
+class GtkPost {
+	Post post;
+
+	this(Post post) {
+		this.post = post;
+	}
+
+	GtkPostSegment[] segmentize() {
+		string[] tokens = this.tokenize();
+		GtkPostSegment[] segments;
+
+		GtkPostSegmentContext context;
+		foreach (int i, string sub; tokens) {
+			GtkPostSegment segment;
+
+			bool is_clock = false;
+
+			foreach (Clock post_clock; this.post.clocks) {
+				if (sub.strip == post_clock.text) {
+					context.clock = true;
+				}
+			}
+
+			switch (sub.strip()) {
+				case "<b>":
+					context.bold = true;
+					break;
+				case "</b>":
+					context.bold = false;
+					break;
+				case "<i>":
+					context.italic = true;
+					break;
+				case "</i>":
+					context.italic = false;
+					break;
+				case "<u>":
+					context.underline = true;
+					break;
+				case "</u>":
+					context.underline = false;
+					break;
+				case "<s>":
+					context.strike = true;
+					break;
+				case "</s>":
+					context.strike = false;
+					break;
+				default:
+					segment.text ~= sub.dup;
+					break;
+			}
+
+			segment.context = context;
+
+			if (segment.text) {
+				segments ~= segment;
+			}
+
+			if (context.clock) {
+				context.clock = false;
+			}
+		}
+
+		return segments;
+	}
+
+	// This is the same function as for the curses interface, for now...
+	string[] tokenize() {
+		string line = this.post.message.replace(regex(`\s+`, "g"), " ");
+
+		// Since I can't use backreferences here...
+		line = line.replace(regex(`<a href="(.*?)".*?>(.*?)</a>`, "g"), "<$1>");
+		line = line.replace(regex(`<a href='(.*?)'.*?>(.*?)</a>`, "g"), "<$1>");
+
+		line = std.array.replace(line, "&lt;", "<");
+		line = std.array.replace(line, "&gt;", ">");
+		line = std.array.replace(line, "&amp;", "&");
+
+		string[] tokens = [""];
+
+		bool next = false;
+		foreach (char c; line) {
+			switch (c) {
+				case '<':
+					tokens ~= "";
+					break;
+				case '{':
+				case '[':
+				case '(':
+				case ' ':
+				case ',':
+					tokens ~= "";
+					next = true;
+					break;
+				case ']':
+				case '>':
+					next = true;
+					break;
+				default:
+					break;
+			}
+
+			tokens[$-1] ~= c;
+
+			if (next) {
+				tokens ~= "";
+				next = false;
+			}
+		}
+
+		return tokens;
+	}
+}
+
---
+++ src/dcc/gtkd/tribuneviewer.d
@@ -0,0 +1,82 @@
+module dcc.gtkd.tribuneviewer;
+
+private import gtk.TextView;
+private import gtk.TextBuffer;
+private import gtk.TextIter;
+private import gtk.TextMark;
+private import gtkc.gtktypes;
+private import gdk.Color;
+
+private import dcc.engine.tribune;
+private import dcc.gtkd.post;
+
+class TribuneViewer : TextView {
+	private TextMark begin, end;
+
+	this() {
+		this.setEditable(false);
+		this.setCursorVisible(false);
+		this.setWrapMode(WrapMode.WORD);
+		this.setIndent(-10);
+
+		TextBuffer buffer = this.getBuffer();
+
+		buffer.createTag("mainclock", "foreground-gdk", new Color(50, 50, 50));
+		buffer.createTag("login", "weight", PangoWeight.BOLD, "foreground-gdk", new Color(0, 0, 100));
+		buffer.createTag("clock", "weight", PangoWeight.BOLD, "underline", PangoUnderline.SINGLE, "foreground-gdk", new Color(0, 0, 100));
+
+		buffer.createTag("b", "weight"       , PangoWeight.BOLD);
+		buffer.createTag("i", "style"        , PangoStyle.ITALIC);
+		buffer.createTag("u", "underline"    , PangoUnderline.SINGLE);
+		buffer.createTag("s", "strikethrough", true);
+	}
+
+	void renderPost(GtkPost post) {
+		string[] tokens = post.tokenize();
+		GtkPostSegment[] segments = post.segmentize();
+
+		TextBuffer buffer = this.getBuffer();
+		TextIter iter = new TextIter();
+		buffer.getEndIter (iter);
+
+		if (buffer.getCharCount() > 1) {
+			buffer.insert (iter, "\n");
+		}
+
+		buffer.insertWithTagsByName(iter, post.post.clock, ["mainclock"]);
+		buffer.insert(iter, " ");
+		buffer.insertWithTagsByName(iter, post.post.login, ["login"]);
+		buffer.insert(iter, " ");
+
+		TextMark mark = buffer.createMark(post.post.post_id, iter, true);
+
+		foreach (GtkPostSegment segment; segments) {
+			TextMark startMark = buffer.createMark("start", iter, true);
+			TextMark endMark = buffer.createMark("start", iter, false);
+
+			buffer.insert(iter, segment.text);
+
+			TextIter startIter = new TextIter();
+			buffer.getIterAtMark(startIter, startMark);
+
+			if (segment.context.bold) {
+				buffer.applyTagByName("b", startIter, iter);
+			}
+
+			if (segment.context.clock) {
+				buffer.applyTagByName("clock", startIter, iter);
+			}
+		}
+
+		TextMark endMark = buffer.createMark("end-" ~ post.post.post_id, iter, true);
+
+		if (!this.begin) {
+			this.begin = mark;
+		}
+
+		if (!this.end) {
+			this.end = endMark;
+		}
+	}
+}
+
