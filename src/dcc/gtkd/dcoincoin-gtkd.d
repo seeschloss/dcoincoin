e7c4294178dd8447f87262e930339adda93ec9cf
SeeSchloss <mvalleton@noparking.net>
2015-01-28 18:09:03 +0100

    Initial GTK UI

--- src/dcc/gtkd/dcoincoin-gtkd.d
+++ src/dcc/gtkd/dcoincoin-gtkd.d
@@ -11,6 +11,22 @@ private import core.thread;
 private import gtk.Main;
 private import gtk.Label;
 private import gtk.MainWindow;
+private import gtk.Menu;
+private import gtk.MenuBar;
+private import gtk.MenuItem;
+private import gtk.AccelGroup;
+private import gtk.Paned;
+private import gtk.Statusbar;
+private import gtk.MessageDialog;
+private import gtk.Box;
+private import gtk.TreeView;
+private import gtk.TreeViewColumn;
+private import gtk.TreeIter;
+private import gtk.TreeSelection;
+private import gtk.CellRendererText;
+private import gtk.ListStore;
+
+private import gtkc.gtktypes;
 
 private import dcc.engine.conf;
 private import dcc.engine.tribune;
@@ -67,10 +83,6 @@ class GtkUI : MainWindow {
 
 	this(string config_file) {
 		super("DCoinCoin");
-		setBorderWidth(10);
-		add(new Label("Plop"));
-
-		showAll();
 
 		this.config_file = config_file;
 
@@ -84,6 +96,90 @@ class GtkUI : MainWindow {
 				stderr.writeln("Posts fetched");
 			});
 		}
+
+		this.setup();
+		this.showAll();
+	}
+
+	void setup() {
+		this.setBorderWidth(10);
+
+		Box mainBox = new Box(GtkOrientation.VERTICAL, 0);
+		mainBox.packStart(makeMenuBar(), false, false, 0);
+
+		Paned paned = new Paned(GtkOrientation.HORIZONTAL);
+
+		paned.add1(this.makeTribunesList());
+		paned.add2(new Label("Add2"));
+
+		mainBox.packStart(paned, true, true, 0);
+
+		Statusbar statusbar = new Statusbar();
+		mainBox.packStart(statusbar, false, false, 0);
+
+		this.add(mainBox);
+	}
+
+	TreeView makeTribunesList() {
+		ListStore listStore = new ListStore([GType.STRING]);
+
+		TreeIter iterTop = listStore.createIter();
+		
+		// Please don't be too smart, you're making it difficult to use loops
+		listStore.remove(iterTop);
+
+		foreach (GtkTribune tribune; this.tribunes) {
+			listStore.append(iterTop);
+			listStore.set(iterTop, [0], [tribune.tribune.name]);
+		}
+
+		TreeView tribunesList = new TreeView(listStore);
+		tribunesList.setHeadersVisible(false);
+		tribunesList.setRulesHint(false);
+
+		TreeSelection ts = tribunesList.getSelection();
+		ts.setMode(SelectionMode.MULTIPLE);
+
+		TreeViewColumn column = new TreeViewColumn("Tribune", new CellRendererText(), "text", 0);
+		tribunesList.appendColumn(column);
+		column.setResizable(false);
+		column.setReorderable(true);
+		column.setSortColumnId(0);
+		column.setSortIndicator(false);
+
+		return tribunesList;
+	}
+
+	MenuBar makeMenuBar() {
+		AccelGroup accelGroup = new AccelGroup();
+		this.addAccelGroup(accelGroup);
+
+		MenuBar menuBar = new MenuBar();
+		Menu menu = menuBar.append("_Tribunes");
+		menu.append(new MenuItem(&onMenuActivate, "_Settings", "tribunes.settings", true, accelGroup, 's'));
+		menu.append(new MenuItem(&onMenuActivate, "E_xit", "application.exit", true, accelGroup, 'x'));
+
+		menu = menuBar.append("_Help");
+		menu.append(new MenuItem(&onMenuActivate, "_About", "help.about", true, accelGroup, 'a', GdkModifierType.CONTROL_MASK | GdkModifierType.SHIFT_MASK));
+
+		return menuBar;
+	}
+
+	void onMenuActivate(MenuItem menuItem) {
+		string action = menuItem.getActionName();
+		switch (action) {
+			default:
+				MessageDialog d = new MessageDialog(
+					this,
+					GtkDialogFlags.MODAL,
+					MessageType.INFO,
+					ButtonsType.OK,
+					"You pressed menu item "~action);
+				d.run();
+				d.destroy();
+			break;
+		}
+
 	}
 }
 
